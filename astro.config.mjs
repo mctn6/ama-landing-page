e97f86b230338029dadeae557455c62bb50316da
Michelle Tan <dev.michelletan@gmail.com>
2024-04-10 11:36:41 +0700

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import purgecss from "astro-purgecss";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [purgecss()]
+  site: 'https://amacontractor.com',
+  integrations: [purgecss(), sitemap()]
 });
